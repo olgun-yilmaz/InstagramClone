4a4d9d7d7f930caea193fd4fd1e6ec606e23a6e7
olgun-yilmaz <olgunyilm@gmail.com>
2025-02-10 10:10:53 +0300

    Initial commit

---
+++ app/src/main/java/com/olgunyilmaz/instagramclonekotlin/UploadActivity.kt
@@ -0,0 +1,27 @@
+package com.olgunyilmaz.instagramclonekotlin
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.olgunyilmaz.instagramclonekotlin.databinding.ActivityUploadBinding
+
+class UploadActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityUploadBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun uploadClicked(view : View){
+
+    }
+
+    fun selectImage(view : View){
+
+    }
+}
