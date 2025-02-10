b2d6ae4edae56e05edaf56eed576ffa7d2905ba8
olgun-yilmaz <olgunyilm@gmail.com>
2025-02-10 10:50:58 +0300

    Add options menu and sign out functionality

--- app/src/main/java/com/olgunyilmaz/instagramclonekotlin/FeedActivity.kt
+++ app/src/main/java/com/olgunyilmaz/instagramclonekotlin/FeedActivity.kt
@@ -1,21 +1,52 @@
 package com.olgunyilmaz.instagramclonekotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.olgunyilmaz.instagramclonekotlin.databinding.ActivityFeedBinding
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var binding : ActivityFeedBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        auth = Firebase.auth
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.insta_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_post){
+            val intent = Intent(this@FeedActivity,UploadActivity :: class.java)
+            startActivity(intent)
+
+        }else if (item.itemId == R.id.sign_out){
+            auth.signOut()
+            val intent = Intent(this@FeedActivity,MainActivity :: class.java)
+            startActivity(intent)
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
