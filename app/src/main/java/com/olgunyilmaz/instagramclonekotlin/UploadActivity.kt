51833375927c0209182a087c3d18565caf5c7e0a
olgun-yilmaz <olgunyilm@gmail.com>
2025-02-10 12:26:15 +0300

    Implement post saving with image upload to Firebase Storage and data to Firestore

--- app/src/main/java/com/olgunyilmaz/instagramclonekotlin/UploadActivity.kt
+++ app/src/main/java/com/olgunyilmaz/instagramclonekotlin/UploadActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
@@ -20,16 +21,26 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.storage
 import com.olgunyilmaz.instagramclonekotlin.databinding.ActivityUploadBinding
+import java.net.URI
+import java.util.UUID
 
 class UploadActivity : AppCompatActivity() {
     private lateinit var binding : ActivityUploadBinding
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
 
-    var selectedBitmap : Bitmap? = null
+    var selectedPicture : Uri? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,9 +51,50 @@ class UploadActivity : AppCompatActivity() {
 
         registerLauncher()
 
+        auth = Firebase.auth
+        firestore = Firebase.firestore
+        storage = Firebase.storage
+
     }
 
     fun uploadClicked(view : View){
+        if (selectedPicture != null){
+            val reference = storage.reference
+            val uuid = UUID.randomUUID()
+            val imgName = "${uuid}.png"
+
+            val imageReference = reference.child("images").child(imgName)
+
+            imageReference.putFile(selectedPicture!!).addOnSuccessListener {
+                // get download url -> firestore
+                imageReference.downloadUrl.addOnSuccessListener { uri ->
+                    val downloadUrl = uri.toString()
+
+                    val postMap = hashMapOf<String,Any>()
+
+                    postMap.put("downloadUrl",downloadUrl)
+                    postMap.put("email",auth.currentUser!!.email!!.toString())
+                    postMap.put("comment",binding.commentText.text.toString())
+                    postMap.put("date",Timestamp.now())
+
+                    firestore.collection("Posts").add(postMap).addOnSuccessListener {
+                        finish()
+                    }.addOnFailureListener {
+                        Toast.makeText(this@UploadActivity,it.localizedMessage,Toast.LENGTH_LONG).show()
+                    }
+
+                }.addOnFailureListener {
+                    Toast.makeText(this@UploadActivity, it.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+
+
+            }.addOnFailureListener {
+                Toast.makeText(this@UploadActivity, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
 
     }
 
@@ -85,18 +137,8 @@ class UploadActivity : AppCompatActivity() {
                 if (intentFromResult != null){
                     val imageURI = intentFromResult.data
                     if (imageURI != null){
-                        try{
-                            if (Build.VERSION.SDK_INT >= 28){
-                                val source = ImageDecoder.createSource(this@UploadActivity.contentResolver, imageURI)
-                                selectedBitmap = ImageDecoder.decodeBitmap(source)
-                            }else{
-                                selectedBitmap = MediaStore.Images.Media.getBitmap(this@UploadActivity.contentResolver,imageURI)
-                            }
-                            binding.imageView.setImageBitmap(selectedBitmap)
-
-                        }catch (e : Exception){
-                            e.printStackTrace()
-                        }
+                        selectedPicture = imageURI
+                        binding.imageView.setImageURI(selectedPicture)
                     }
                 }
             }
